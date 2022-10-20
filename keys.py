ae12028e916072bc19897c1ebfcd8cc7ff785ad4
owendecicco1 <owen_decicco1@baylor.edu>
2022-10-20 15:30:47 -0500

    wordcloud

--- keys.py
+++ keys.py
@@ -1,11 +1,11 @@
 consumer_key = 'Mpq99uFFzn8fyj9lPVQZb7rEs'
 consumer_secret = 'DpsbC4t76HVGXmnW8Tlh0ypYTOSS9ValCWcKAtNYQ4g5vXGLGv'
-access_token = '2190430531-HX94SgcxHIJtNKDnEcKTGihkOueACS8ZAQUA1yh'
-access_token_secret = 'cPya7vG3lw3W3LTPG87m9VYro9T42mblkyXM8Y4buF6WW'
+access_token = '1176717085717778434-LvWvvp4UN03ZX2rVnHKO4ZTijDBgMl'
+access_token_secret = '66qxcUCavE8lkUFaN5W8zok6xMKWv8ASILhNlJnTIphZN'
 
 mapquest_key = 'YourAPIKey'
 
 
-api_key =  'ERLlmRmvt26YebhbmafevvUlB'
-api_key_secret = '0rL3N8CIItQF8MBCu5zruJYqGE7Ki9NYPTEawkML9BlS6Y8waB'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMKjCgEAAAAAEWqvx6mE3RoX4%2FMmX9Pmk8fWwuA%3DEApIMyOqcDhcf2iwBoL20glObsuADw0TKoEu43jlWqGlpKagIU'
+api_key =  'bMQut5rC0koQkgrvlXa24USLI'
+api_key_secret = 'mdAQqV2uRsz6kOLEo1vH3hu0wXsIP3B1uzXPvLCrbgGdVPthmy'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAF6SiQEAAAAAJu3Y7C5%2F0K%2BP4uFkQUQaZxeVelg%3DuUafeQIOMggUfFnOC8jO5n7xxf8N07FVWT6TC04un8GGBjvArz'
